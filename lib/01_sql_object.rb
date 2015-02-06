e2f5db6e6194d584d0ca9373ca38d4bbfac353aa
Jacob Shorty <jakeshorty@gmail.com>
2015-02-06 13:37:29 -0500

    Add #insert method to create new records in database

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -84,11 +84,21 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |attr_name| self.send(attr_name) }
   end
 
   def insert
-    # ...
+    col_names = self.class.columns.join(",")
+    question_marks = (["?"] * self.class.columns.length).join(",")
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
