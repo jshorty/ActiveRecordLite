94c379ddba3af7a722afab6adf6c8aa49f17dc5e
Jacob Shorty <jakeshorty@gmail.com>
2015-02-06 14:46:12 -0500

    Add initialize methods for BelongsToOptions, HasManyOptions; add table access methods

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,23 +10,39 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      class_name: name.to_s.singularize.camelcase,
+      foreign_key: "#{name}_id".to_sym,
+      primary_key: :id
+    }.merge!(options)
+
+    self.class_name = defaults[:class_name]
+    self.foreign_key = defaults[:foreign_key]
+    self.primary_key = defaults[:primary_key]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      class_name: name.to_s.singularize.camelcase,
+      foreign_key: "#{self_class_name.to_s.underscore}_id".to_sym,
+      primary_key: :id
+    }.merge!(options)
+
+    self.class_name = defaults[:class_name]
+    self.foreign_key = defaults[:foreign_key]
+    self.primary_key = defaults[:primary_key]
   end
 end
 
