47dcabc359847331c596809a91408eaff0e02281
Jacob Shorty <jakeshorty@gmail.com>
2015-03-25 13:57:29 -0400

    Refactor into several simpler modules

--- cats.sql
+++ cats.sql
@@ -28,16 +28,16 @@ VALUES
 INSERT INTO
   humans (id, fname, lname, house_id)
 VALUES
-  (1, "Devon", "Watts", 1),
-  (2, "Matt", "Rubens", 1),
-  (3, "Ned", "Ruggeri", 2),
+  (1, "Alan", "Watts", 1),
+  (2, "Friedrich", "Nietzsche", 1),
+  (3, "Jake", "Shorty", 2),
   (4, "Catless", "Human", NULL);
 
 INSERT INTO
   cats (id, name, owner_id)
 VALUES
-  (1, "Breakfast", 1),
+  (1, "Lunch", 1),
   (2, "Earl", 2),
   (3, "Haskell", 3),
-  (4, "Markov", 3),
+  (4, "Lamar", 3),
   (5, "Stray Cat", NULL);
--- lib/associatable.rb
+++ lib/associatable.rb
@@ -1,7 +1,6 @@
-require_relative '02_searchable'
+require_relative 'searchable'
 require 'active_support/inflector'
 
-# Phase IIIa
 class AssocOptions
   attr_accessor(
     :foreign_key,
@@ -47,7 +46,6 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
     options = BelongsToOptions.new(name, options)
     assoc_options[name] = options
